737ebf6174fe47cec8514743a72117de8c41b188
beirao <36646641+beirao@users.noreply.github.com>
2020-02-25 16:22:07 +0100

    Update matrice_led.py

--- Matrice_led/matrice_led.py
+++ Matrice_led/matrice_led.py
@@ -3,18 +3,50 @@ from microbit import *
 
 
 
-emoji_content = Image("05050:"
-                      "09090:"
-                      "00000:"
-                      "90009:"
-                      "09990")
+while True :
 
-display.show(boat)
-                      
-emoji_content = Image("05000:"
-                      "09050:"
-                      "00000:"
-                      "90009:"
-                      "09990")
+    
+    inclinaison = accelerometer.get_x()
+    
+    if button_a.is_pressed() and (inclinaison > 200 or inclinaison <-200):
+        emoji_content = Image("00000:"
+                              "09990:"
+                              "00000:"
+                              "09990:"
+                              "90009")
+        
+        display.show(emoji_content)
+        sleep(1500)
+    
+    elif button_a.is_pressed():
+        emoji_content = Image("05050:"  #display.show(Image.HAPPY)
+                              "09090:"
+                              "00000:"
+                              "90009:"
+                              "09990")
+        
+        display.show(emoji_content)
+        sleep(100)
+                              
+        emoji_content = Image("05000:"
+                              "09050:"
+                              "00000:"
+                              "90009:"
+                              "09990")
+        
+        display.show(emoji_content)
+        sleep(100)
+    
 
-display.show(boat)
+    else :
+        display.clear()
+        
+    #---------------
+    
+    inclinaison = accelerometer.get_x()
+    if inclinaison > 20:
+        display.show("D")
+    elif inclinaison < -20:
+        display.show("G")
+    else:
+        display.show("-")
